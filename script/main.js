2193380b7112f8065f640da798deb10f8cc09f81
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-09 18:47:08 +0200

    first step logic JS (to do corousel img)

--- script/main.js
+++ script/main.js
@@ -1,1 +1,28 @@
-console.log('js work');
+$(document).ready(function() {
+
+
+    // refs
+    var leftClick = $('.icon-left > a'); //click sinistro
+    var rightClick = $('.icon-right > a'); //click destro
+    console.log(rightClick);
+    
+
+    console.log(rightClick);
+
+    // ref img
+    var firstImg = $('.first-img');
+    console.log(firstImg);
+    
+
+    rightClick.click(function() {
+        firstImg.hide()
+    });
+
+
+
+
+
+
+    //end ready function 
+});
+
