b37aadd326767a3e5df74565f6aa6666e5d586fd
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-09 22:07:59 +0200

    add total control image left and right + active-circle (done js)

--- script/main.js
+++ script/main.js
@@ -5,28 +5,73 @@ $(document).ready(function() {
     var leftClick = $('.icon-left > a'); //click sinistro
     var rightClick = $('.icon-right > a'); //click destro
     console.log(rightClick);
+
+    // ref img
+
+    var firstImg = $('.first');
+    var lastImg = $('.last');
+
+    var firstCircle = $('.first_circle');
+    var lastCircle = $('.last_circle');
+    
+    
+  
     
 
     leftClick.click(function() {
         console.log('click Sinistra');
+        // img ref
         var currentImg = $('.image-box img.active');
+        // circle ref
 
-        if (currentImg.hasClass('active')) {
-            currentImg.removeClass('active')
-            currentImg.prev().addClass('active')
-        } 
+        var currentCircle = $('.circle-box .circle.active_circle')
+        
+        // img change
+        currentImg.removeClass('active')
+        currentImg.prev().addClass('active') 
 
+        // scorrimento img
+        if (currentImg.hasClass('first')) {
+            lastImg.addClass('active')
+        }
+        
+        //scorrimento circle
+
+        currentCircle.removeClass('active_circle');
+        currentCircle.prev().addClass('active_circle')
+
+        if (currentCircle.hasClass('first_circle')) {
+            lastCircle.addClass('active_circle')
+        }
+       
     });
 
     
     rightClick.click(function() {
         console.log('click Destra');
+        // img ref
         var currentImg = $('.image-box img.active');
 
-        if (currentImg.hasClass('active')) {
-            currentImg.removeClass('active')
-            currentImg.next().addClass('active')
-        } 
+        //circle ref
+        var currentCircle = $('.circle-box .circle.active_circle')
+
+        // scorrimento img
+
+        currentImg.removeClass('active')
+        currentImg.next().addClass('active')
+        
+        if (currentImg.hasClass('last')) {
+            firstImg.addClass('active')
+        }
+
+        // scorrimento circle
+
+        currentCircle.removeClass('active_circle');
+        currentCircle.next().addClass('active_circle')
+
+        if (currentCircle.hasClass('last_circle')) {
+            firstCircle.addClass('active_circle')
+        }
 
     });
 
