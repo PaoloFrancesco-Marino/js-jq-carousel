e006623bc2360f22ca0f4fad9323ceb821c5d629
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-10 14:23:01 +0200

    add control key Up and Key Down

--- script/main.js
+++ script/main.js
@@ -4,15 +4,44 @@ $(document).ready(function() {
     var rightClick = $('.icon-right > a'); //click destro
     console.log(rightClick);
 
+    var upClick = $('.icon-up > a') //click in alto
+    var downClick = $('.icon-down > a') //click in baso
+
     // refs first/last Item
 
     var first = $('.first');
     var last = $('.last');
     
+    var firstBox = $('.first-box')
+    var lastBox = $('.last-box')
+    
     // refs addClass
 
     var activeImg = 'active';
+    var activeBox = 'active';
     var activeCircle = 'active_circle';
+    var imageFirst = 'first';
+    var imageLast = 'last';
+    var initialBox = 'first-box';
+    var finishBox = 'last-box';
+
+    // up click (cambia categoria img)
+    upClick.click(function() {
+        console.log('click in alto');
+
+        currentBoxImage = $('.image-box.active')
+        prevItem(currentBoxImage, lastBox, activeBox, initialBox);
+        // end down click
+    });
+
+    // down Click (cambia categoria img)
+    downClick.click(function() {
+        console.log('click in basso');
+
+        currentBoxImage = $('.image-box.active')
+        nextItem(currentBoxImage, firstBox, activeBox, finishBox);
+        // end down click
+    });
   
     // left click
     leftClick.click(function() {
@@ -24,8 +53,8 @@ $(document).ready(function() {
         // circle ref
         var currentCircle = $('.nav-box .circle.active_circle')
         
-        prevItem(currentImg, last, activeImg);
-        prevItem(currentCircle, last, activeCircle);
+        prevItem(currentImg, last, activeImg, imageFirst);
+        prevItem(currentCircle, last, activeCircle, imageFirst);
 
         // end left click
     });
@@ -37,10 +66,10 @@ $(document).ready(function() {
         var currentImg = $('.image-box img.active');
 
         //circle ref
-        var currentCircle = $('.nav-box .circle.active_circle')
+        var currentCircle = $('.nav-box .circle.active_circle');
 
-        nextItem(currentImg, first, activeImg);
-        nextItem(currentCircle, first, activeCircle) 
+        nextItem(currentImg, first, activeImg, imageLast);
+        nextItem(currentCircle, first, activeCircle, imageLast);
 
         // end right click
     });
@@ -55,19 +84,27 @@ $(document).ready(function() {
         //circle ref
         var currentCircle = $('.nav-box .circle.active_circle')
 
+        // box image
+        currentBoxImage = $('.image-box.active')
+
         // keycode event push
         var keyCaps = event.keyCode;
-
-        if (keyCaps == 37) {
+        if (keyCaps == 38) {
+            // up click
+            prevItem(currentBoxImage, lastBox, activeBox, initialBox);
+        } else if (keyCaps == 40) {
+            // down click
+            nextItem(currentBoxImage, firstBox, activeBox, finishBox);
+        } else if (keyCaps == 37) {
             // keydown left
-            prevItem(currentImg, last, activeImg);
-            prevItem(currentCircle, last, activeCircle);
+            prevItem(currentImg, last, activeImg, imageFirst);
+            prevItem(currentCircle, last, activeCircle, imageFirst);
             
 
         } else if (keyCaps == 39) {
             // // keydown right
-            nextItem(currentImg, first, activeImg);
-            nextItem(currentCircle, first, activeCircle) 
+            nextItem(currentImg, first, activeImg, imageLast);
+            nextItem(currentCircle, first, activeCircle, imageLast);
         }
         // end keyword action
     });
@@ -81,25 +118,25 @@ $(document).ready(function() {
  */
 
 // prev item (prende l'oggetto precedente)
-function prevItem (currentItem, lastItem, classItem) {
+function prevItem (currentItem, lastItem, classItem, hasClass) {
 
     // img change
     currentItem.removeClass(classItem)
     currentItem.prev().addClass(classItem) 
 
     // scorrimento img
-    if (currentItem.hasClass('first')) {
+    if (currentItem.hasClass(hasClass)) {
         lastItem.addClass(classItem)
     }    
 }
 
 // next item (prende l'ogetto successivo)
-function nextItem (currentItem, firstItem, classItem) {
+function nextItem (currentItem, firstItem, classItem, hasClass) {
 
     currentItem.removeClass(classItem)
     currentItem.next().addClass(classItem)
     
-    if (currentItem.hasClass('last')) {
+    if (currentItem.hasClass(hasClass)) {
         firstItem.addClass(classItem)
     }
 }
